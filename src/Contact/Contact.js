58441f35f5b6f881b08734a3694a43d92ef9f99b
haley-r <haleyryan77@gmail.com>
2020-02-18 10:56:30 -0600

    base mode

---
+++ src/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <section id="contact-section">
+                <h2>Contact</h2>
+                    <p>Phone: 1-800-We-REACT</p>
+                    <p>Email: info@ReactActors.com</p>
+            </section>
+        );
+    }
+}
+
+export default Contact;
